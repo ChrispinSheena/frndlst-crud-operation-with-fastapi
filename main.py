bf4acb41db039399fe0d081e868a4fbe58c5b4eb
ChrispinSheena <chrispinsheena.25it@licet.ac.in>
2024-07-31 10:17:41 +0530

    modified  main.py

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
+import os
 
 
 app = FastAPI()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", 3307)
+DB_NAME = os.getenv("DB_NAME")
 
 
-SQLdb = "mysql+pymysql://root:root@localhost/db"
-engine = create_engine(SQLdb)
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
